eefc57a0039b03cd4e96b5033238083f369a675b
Łukasz Borawski <lukasz.borawski@gmail.com>
2022-05-09 21:34:41 +0200

    deploy5

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <Wrapper>
       <ContentWrapper>
-        <Title>Where in the World?</Title>
+        <Title>Where in the world?</Title>
         <DarkModeButton onClick={toggleTheme}>
           <Icon />
           <ButtonText>Dark Mode</ButtonText>
